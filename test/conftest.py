2e54540f9b4f42692b131ad7bd8f7e66dea31461
Gizem Ekinci <ekinci@dkrz.de>
2025-10-10 13:21:07 +0200

    Updated

--- test/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-@pytest.fixture(scope="session")
-def anyio_backend():
-    # Run anyio-marked tests on asyncio only
-    return "asyncio"
-    # or: return ["asyncio"]  # also acceptable

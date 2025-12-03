e494e8d03334c7826075388447621ced66267fab
Gizem Ekinci <ekinci@dkrz.de>
2025-12-03 15:27:10 +0100

    Added new endpoints to router

--- src/api/chatbot/__init__.py
+++ src/api/chatbot/__init__.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter
 from .availablechatbots import router as availablechatbots_router
 from .getthread import router as getthread_router
 from .getuserthreads import router as getuserthreads_router
+from .deletethread import router as deletethread_router
+from .setthreadtopic import router as setthreadtopic_router
 from .streamresponse import router as streamresponse_router
 from .stop import router as stop_router
 from .heartbeat import router as heartbeat_router
@@ -13,6 +15,8 @@ router = APIRouter()
 router.include_router(availablechatbots_router)
 router.include_router(getthread_router)
 router.include_router(getuserthreads_router)
+router.include_router(deletethread_router)
+router.include_router(setthreadtopic_router)
 router.include_router(streamresponse_router)
 router.include_router(stop_router)
 router.include_router(heartbeat_router)

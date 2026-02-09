db65402d8563e5d4a56d7e0da8d936b70cf4cebe
Gizem Ekinci <ekinci@dkrz.de>
2026-02-09 16:54:30 +0100

    Added HTTP error for MongoDB connection and failuere to save new thread with edited input

--- src/api/chatbot/editthread.py
+++ src/api/chatbot/editthread.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException
-from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 from src.services.service_factory import Authenticator, AuthRequired, auth_dependency, get_thread_storage
 from src.services.streaming.stream_variants import from_json_to_sv
@@ -20,14 +19,23 @@ async def edit_thread(
     user_name = Auth.username
     vault_url = Auth.vault_url
 
+    if not source_thread_id:
+        raise HTTPException(
+            status_code=422,
+            detail="Source thread ID not found. Please provide thread_id in the query parameters.",
+        )
+
     if not vault_url:
         raise HTTPException(
-            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            status_code=422,
             detail="Vault URL not found in headers",
         )
 
-    # Thread storage 
-    Storage = await get_thread_storage(vault_url=Auth.vault_url)
+    try:
+        # Thread storage 
+        Storage = await get_thread_storage(vault_url=Auth.vault_url)
+    except:
+        raise HTTPException(status_code=503, detail="Failed to connect to MongoDB.")
 
     # Load original content
     try:
@@ -40,7 +48,7 @@ async def edit_thread(
     # Check index within bounds
     if fork_from_index < 0 or fork_from_index >= len(orig_json):
         raise HTTPException(
-            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            status_code=422,
             detail="fork_from_index outside content range! Please review query parameters!",
         )
 
@@ -52,15 +60,18 @@ async def edit_thread(
 
     new_id = await new_thread_id()
     root_thread_id = source_thread_id  # TODO: if there are many changes we need to track down the original root
-
-    await Storage.save_thread(
-        thread_id=new_id,
-        user_id=user_name,
-        content=base_sv,
-        root_thread_id=root_thread_id,
-        parent_thread_id=source_thread_id,
-        fork_from_index= fork_from_index,
-    )
+    
+    try:
+        await Storage.save_thread(
+            thread_id=new_id,
+            user_id=user_name,
+            content=base_sv,
+            root_thread_id=root_thread_id,
+            parent_thread_id=source_thread_id,
+            fork_from_index= fork_from_index,
+        )
+    except:
+        raise HTTPException(status_code=500, detail="Failed to save new thread with edited user input.")
 
     # Return the new thread_id and the base history
     return {

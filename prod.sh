3c3efeabb3b853b4dc75983ef5fd7de0de0a0bf7
Gizem Ekinci <ekinci@dkrz.de>
2026-03-11 15:09:59 +0100

    Bugfix typo

--- prod.sh
+++ prod.sh
@@ -8,6 +8,6 @@ COMPOSE_FILE="docker-compose.yml"
 echo "[prod.sh] Generating scaled compose file and nginx-conf from ${COMPOSE_FILE}"
 ./gen_compose.py ${COMPOSE_FILE}
 
-echo "[prod.sh] podman-compose -f "docker-compose.scale.yml" $*"
+echo "[prod.sh] podman-compose -f "docker-compose.scaled.yml" $*"
 
-podman-compose -f "docker-compose.scale.yml" "$@"
+podman-compose -f "docker-compose.scaled.yml" "$@"
